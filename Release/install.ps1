c50cb8f3215d2df52325faaaf5df5e833f535589
YuanWei <420396633@qq.com>
2025-08-05 15:00:50 +0800

    install.ps1和uninstall_webstorm.ps1执行文件

---
+++ Release/install.ps1
@@ -0,0 +1,6 @@
+﻿# 设置控制台编码为UTF-8防止中文乱码
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
+$releasePath = (resolve-path .)
+Add-AppxPackage ${releasePath}\apex-sparse.appx -ExternalLocation ${releasePath}
